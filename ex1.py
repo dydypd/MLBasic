aab3dc74a645dc0a82697bbd9944229afe1072ed
dydypd <88722867+dydypd@users.noreply.github.com>
2023-02-15 20:32:15 +0700

    Initial commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.colors as colors
+import seaborn as sns
+import itertools
+from scipy.stats import norm
+import scipy.stats
+from sklearn.naive_bayes import GaussianNB
